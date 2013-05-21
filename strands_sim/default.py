6fc8a39e6f99152a670086dc6f00ba02015be512
Lars Kunze <kula78@gmail.com>
2013-05-21 09:54:04 +0100

    Moved sensors and actuators into robot specification

--- strands_sim/default.py
+++ strands_sim/default.py
@@ -11,39 +11,10 @@ from strands_sim.builder.robots import Scitosa5
 #robot = Ranger()
 robot = Scitosa5()
 
-# The list of the main methods to manipulate your components
-# is here: http://www.openrobots.org/morse/doc/stable/user/builder_overview.html
-robot.add_interface('ros')
-
 # tum_kitchen
 robot.translate(x=2.5, y=3.2, z=0.0)
 # sandbox
 #robot.translate(1.0, 0.0, 0.0)
-
-odometry = Odometry()
-robot.append(odometry)
-odometry.add_interface('ros', topic="/odom")
-
-scan = Hokuyo()
-#scan.translate(x=0.275, z=0.252)
-#scan.translate(x=0.65, z=-0.60)
-scan.translate(x=0.30, z=0.386)
-robot.append(scan)
-scan.properties(Visible_arc = False)
-scan.properties(laser_range = 30.0)
-scan.properties(resolution = 1.0)
-scan.properties(scan_window = 180.0)
-scan.create_laser_arc()
-
-scan.add_interface('ros', topic='/scan')
-
-# Keyboard control
-keyboard = Keyboard()
-robot.append(keyboard)
-
-motion = MotionXYW()
-robot.append(motion)
-motion.add_interface('ros', topic='/cmd_vel')
 
 # Set the environment
 env = Environment('tum_kitchen/tum_kitchen')
--- strands_sim/src/strands_sim/builder/robots/scitosA5.py
+++ strands_sim/src/strands_sim/builder/robots/scitosA5.py
@@ -11,23 +11,44 @@ class Scitosa5(Robot):
         #Robot.__init__(self, '/home/lars/work/strands/ranger_sim/data/ranger_sim/robots/ranger.blend')
         self.properties(classpath = "strands_sim.robots.scitosA5.Scitosa5")
 
+        # The list of the main methods to manipulate your components
+        # is here: http://www.openrobots.org/morse/doc/stable/user/builder_overview.html
+        self.add_interface('ros')
+
         ###################################
         # Actuators
         ###################################
 
-        # Check here the other available actuators:
-        # http://www.openrobots.org/morse/doc/stable/components_library.html#actuators
-        #self.motion = MotionXYW()
-        #self.append(self.motion)
+        # Motion control
+        self.motion = MotionXYW()
+        self.append(self.motion)
+        self.motion.add_interface('ros', topic='/cmd_vel')
 
-        # Optionally allow to move the robot with the keyboard
-        #if debug:
-        #    keyboard = Keyboard()
-        #    self.append(keyboard)
+        # Keyboard control
+        self.keyboard = Keyboard()
+        self.append(self.keyboard)
 
         ###################################
         # Sensors
         ###################################
+
+        # Odometry
+        self.odometry = Odometry()
+        self.append(self.odometry)
+        self.odometry.add_interface('ros', topic="/odom")
+
+        # Laserscanner
+        self.scan = Hokuyo()
+        #scan.translate(x=0.275, z=0.252)
+        #scan.translate(x=0.65, z=-0.60)
+        self.scan.translate(x=0.30, z=0.386)
+        self.append(self.scan)
+        self.scan.properties(Visible_arc = False)
+        self.scan.properties(laser_range = 30.0)
+        self.scan.properties(resolution = 1.0)
+        self.scan.properties(scan_window = 180.0)
+        self.scan.create_laser_arc()
+        self.scan.add_interface('ros', topic='/scan')
 
         #self.pose = Pose()
         #self.append(self.pose)
