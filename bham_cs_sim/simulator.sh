d53fbba9fed86a38182c12c12de42c2faca5ec92
Chris Burbridge <cburbridge@gmail.com>
2013-05-21 16:31:50 +0100

    fix path error.

--- bham_cs_sim/simulator.sh
+++ bham_cs_sim/simulator.sh
@@ -3,9 +3,10 @@
 # To allow rosrun to launch the morse simulator..
 environment_name="bham_cs_sim"
 
-path=`rospack find strands_sim`
+path=`rospack find $environment_name`
+strands_sim=`rospack find strands_sim`
 #cd $pth
 #cd ..
-added=`$path/morse_config.py $environment_name $path`
+added=`$strands_sim/morse_config.py $environment_name $path`
 echo "Running morse on $path"
 morse run $environment_name
