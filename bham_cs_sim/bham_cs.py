2299b25756513348fb8c8a524a5b8648dcc6d52d
Marc Hanheide <marc@hanheide.net>
2013-07-01 15:35:12 +0100

    refactoring of repository: * added stuff to catkinise this repository (now strands_morse is the package, everythnig else is contained in it) * the policy is that different environments can go into different subdirs (simulator.sh takes care of setting everything up) * changed simulator.sh to set PYTHONPATH etc and removed this from the specific builder scripts * move all non-simulation code (strands_executive) into subfolder TO-BE-MOVED for now * created new environment tum_kitchen * put everything that is common into strands_sim (robots, scripts, etc), make sure other environments can find what is in strands_sim

--- bham_cs_sim/bham_cs.py
+++ bham_cs_sim/bham_cs.py
@@ -8,14 +8,6 @@ import sys
 import subprocess 
 import os
 
-# Add the bham_cs_sim to the python path and to MORSE_RESOURCE_PATH
-pack=subprocess.Popen(["rospack","find","bham_cs_sim"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-bham_cs_path=pack.stdout.read().decode("utf-8").strip()
-sys.path.append(os.path.join(bham_cs_path,"src"))
-resources=os.getenv('MORSE_RESOURCE_PATH',"")
-os.environ["MORSE_RESOURCE_PATH"]=resources+":"+os.path.join(bham_cs_path,"data")
-
-
 from morse.builder import *
 from strands_sim.builder.robots import Scitosa5
 from bham_cs_sim.builder.robots import Elevator
@@ -32,7 +24,7 @@ lift.add_default_interface('socket')
 
 
 # Set the environment
-model_file=os.path.join(bham_cs_path,'data/CS_Building_LG.blend')
+model_file=os.path.join(os.path.dirname(os.path.abspath( __file__ )),'data/CS_Building.blend')
 env = Environment(model_file,fastmode=False)
 env.place_camera([10.0, -10.0, 10.0])
 env.aim_camera([1.05, 0, 0.78])
