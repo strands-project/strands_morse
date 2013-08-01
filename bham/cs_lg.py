376edac22103be1ef47f2541a8182c26ca44d840
Lars Kunze <kula78@gmail.com>
2013-08-01 10:46:20 +0100

    added docking station and label to environment

--- bham/cs_lg.py
+++ bham/cs_lg.py
@@ -12,8 +12,10 @@ from morse.builder import *
 from strands_sim.builder.robots import Scitosa5
 from bham.builder.robots import Elevator
 
-robot = Scitosa5()
-robot.translate(x=-1.35, y=0.56, z=7.5)
+robot = Scitosa5(with_cameras = Scitosa5.WITHOUT_DEPTHCAMS)
+#robot.translate(x=-1.35, y=0.56, z=7.5)
+robot.translate(x=1, y=-6.50, z=1.5)
+robot.rotate(z=1.57)
 
 lift = Elevator()
 lift.translate(3.81419,2.51356,0)
@@ -22,6 +24,14 @@ lift.translate(3.81419,2.51356,0)
 # by lift_controller.py, which in turn provides a ros interface...
 lift.add_default_interface('socket')
 
+docking_station = PassiveObject('strands_sim/robots/docking_station.blend','dockingStation')
+docking_station.properties(ChargingZone = True)
+docking_station.translate(1,-4.65,0.335)
+docking_station.rotate(0,0,1.57)
+
+docking_station_label = PassiveObject('strands_sim/robots/docking_station_label.blend','dockingStationLabel')
+docking_station_label.translate(1,-4.55,1.8)
+docking_station_label.rotate(1.57,0,0)
 
 # Set the environment
 model_file=os.path.join(os.path.dirname(os.path.abspath( __file__ )),'data/cs_lg.blend')
