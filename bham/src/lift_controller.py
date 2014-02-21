1f7977884e3dc2f1bae6be166f72f4e9e5e642c4
cburbridge <cburbridge@gmail.com>
2014-02-21 17:24:14 +0000

    Fix lift controller for python 3.3 install

--- bham/src/lift_controller.py
+++ bham/src/lift_controller.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3.3
 """
 An over simplified lift controller.
 
@@ -15,8 +15,9 @@ The topics correspond to a user pressing the "call" buton on the outside of the 
 import sys
 import rospy
 from std_msgs.msg import Bool
+import pymorse
+import imp
 
-from pymorse import Morse
 rospy.init_node('lift_controller')
 doors=['B','G','1','2']
 
@@ -25,7 +26,7 @@ lift_commands=[0]
 def on_floor_call_command(data, args):
     (type,floor)=args
     """ On the press of the call or inside command button for lift..."""
-    rospy.loginfo("Lift %s: To floor %s"%(type, doors[floor]))
+    rospy.loginfo("[Lift Controller] Lift %s: To floor %s"%(type, doors[floor]))
     
     if floor in lift_commands:
         return
@@ -37,24 +38,34 @@ for t in ["call","command"]:
         rospy.Subscriber("/lift_sim/%s%s"%(t,doors[i]), Bool, on_floor_call_command, callback_args=(t,i))
 
 morse = None
-while morse is None:
-    try:
-        morse = Morse()
-    except ConnectionError as e:# ConnectionError as e:
-        rospy.loginfo("Waiting for Morse...")
-        print("[!!!!!] ",e,"Waiting for Morse...")
-        rospy.sleep(3)
-    
+
 while not rospy.is_shutdown():
-    if len(lift_commands)>1:
-        rospy.loginfo("Closing door %s"%doors[lift_commands[0]])
-        morse.rpc('lift.door%s'%doors[lift_commands[0]],'change_door_state',0)
-        lift_commands=lift_commands[1:]
-        rospy.loginfo("Moving to floor %s"%doors[lift_commands[0]])
-        morse.rpc('lift.platform','move_to_floor',lift_commands[0]-1)
-        rospy.loginfo("Opening door %s"%doors[lift_commands[0]])
-        morse.rpc('lift.door%s'%doors[lift_commands[0]],'change_door_state',1)
-        rospy.sleep(8)
+    rospy.loginfo("[Lift Controller] Waiting for Morse...")
+    if pymorse.Morse._asyncore_thread is not None:
+        # This is a strange hack that is required because of some bug in pymorse.
+        # The async thread created by pymorse needs to be forced to recreate
+        # when we start a new connection.
+        rospy.loginfo("[Lift Controller] Crikey")
+        pymorse.Morse._asyncore_thread.join()
+        pymorse.Morse._asyncore_thread = None
+    try:
+        with pymorse.Morse() as morse:
+
+            rospy.loginfo ("[Lift Controller] Ready.")
+
+            while not rospy.is_shutdown():
+                if len(lift_commands)>1:
+                    rospy.loginfo("[Lift Controller] Closing door %s"%doors[lift_commands[0]])
+                    morse.rpc('lift.door%s'%doors[lift_commands[0]],'change_door_state',0)
+                    lift_commands=lift_commands[1:]
+                    rospy.loginfo("[Lift Controller] Moving to floor %s"%doors[lift_commands[0]])
+                    morse.rpc('lift.platform','move_to_floor',lift_commands[0]-1)
+                    rospy.loginfo("[Lift Controller] Opening door %s"%doors[lift_commands[0]])
+                    morse.rpc('lift.door%s'%doors[lift_commands[0]],'change_door_state',1)
+                    rospy.sleep(8)
+    except Exception as e:
+        rospy.loginfo("[Lift Controller] " + str(e) + " : will retry.")
+        rospy.sleep(0.5)
+    
 
 
-morse.close()
