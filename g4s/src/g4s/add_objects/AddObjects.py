d29261c1ebbe231a51b911bac99d02ee90451fb1
Lenka <lxm210@cs.bham.ac.uk>
2015-03-14 15:46:00 +0000

    g4s simulation extended by population area2 with furniture

--- g4s/src/g4s/add_objects/AddObjects.py
+++ g4s/src/g4s/add_objects/AddObjects.py
@@ -8,32 +8,48 @@ from math import pi
 
 def add_desk_divider():  
 
-  x = [-9.89, -4.72, 0.03, 5.26]
-  y= 3.32
+  x = [-9.89, -4.72, 0.03, 5.26, 18.08]
+  y1 = 3.32
+  y2 = 3.01
   yaw = pi/2 
-  n = 4;
+  n = 5;
  
   for i in range(n):
-    wall = AddDeskDivider(x[i],y,yaw)
+    if i<4:
+      wall = AddDeskDivider(x[i],y1,yaw)
+    else:
+      wall = AddDeskDivider(x[i],y2,yaw)
 
 def add_right_table():
-  n = 9
-  x=[ -13.85, -11.46, -8.32, -6.29, -3.15, -1.54, 1.6, 3.69, 6.83]
-  y= 3.32 
-  yaw =[-pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2] 
+  n = 14
+  x=[ -13.85, -11.46, -8.32, -6.29, -3.15, -1.54, 1.6, 3.69, 6.83, 15.55, 16.51, 19.65, 21.06, 18.09]
+  y1 = 3.32
+  y2 = 3.01 
+  y3 = -4.88
+  yaw =[-pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2] 
 
   for i in range(n):
-    table = AddRightTable(x[i],y,yaw[i])
+    if i<9:
+      table = AddRightTable(x[i],y1,yaw[i])
+    elif i<13:
+      table = AddRightTable(x[i],y2,yaw[i])
+    elif i == 13:
+      table = AddRightTable(x[i],y3,yaw[i])
+
 
 def add_left_table():
-  n = 9
-  x=[ -13.85, -11.46, -8.32, -6.29, -3.15, -1.54, 1.6, 3.69, 6.83]
-  y= 3.32 
-  yaw =[-pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2] 
+  n = 13
+  x=[ -13.85, -11.46, -8.32, -6.29, -3.15, -1.54, 1.6, 3.69, 6.83, 15.55, 16.51, 19.65, 21.06]
+  y1 = 3.32 
+  y2 = 3.01
+  yaw =[-pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2, pi/2, -pi/2, pi/2, -pi/2] 
 
   for i in range(n):
-    table = AddLeftTable(x[i],y,yaw[i])
-  
+    if i<9:
+      table = AddLeftTable(x[i],y1,yaw[i])
+    elif i<13:
+      table = AddLeftTable(x[i],y2,yaw[i])
+
 def add_shelf():  
 
   s = 1.06 # size of the shelf
@@ -47,6 +63,17 @@ def add_shelf():
     shelf = AddShelf(x[i],y,yaw,b[i*3])
     shelf = AddShelf(x[i]-s,y,yaw,b[i*3+1])
     shelf = AddShelf(x[i]-2*s,y,yaw,b[i*3+2])
+
+  x2 = 16.39
+  y2 = -1.06
+  shelf = AddShelf(x2,y2,0,1) 
+  shelf = AddShelf(x2+s,y2,0,0) 
+  shelf = AddShelf(x2,y2-0.5,pi,0) 
+  shelf = AddShelf(x2+s,y2-0.5,pi,0) 
+  
+  
+
+
   
 def add_tall_shelf():
   s = 1.06
@@ -62,7 +89,7 @@ def add_tall_shelf():
     shelf = AddTallShelf(x[i],y[i]-s,yaw[i],b[i*3+1])
     shelf = AddTallShelf(x[i],y[i]-2*s,yaw[i],b[i*3+2])
 
-  shelf = AddTallShelf(-15.27,5.16,pi,1)
+  shelf = AddTallShelf(-14.75,5.06,pi,1)
 
   
 
